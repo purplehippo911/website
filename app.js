58f9b7977dc29f63e5ebe6ec6e0fcfbd35569fd3
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-07-26 16:24:17 +0200

    .
    finished with responsive navbar

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const adviceDiv = document.querySelector(".quote__item");
-     
+const hamburger = document.querySelector(".hamburger");
+const dropdown = document.querySelector(".dropdown");
+
 const fetchAdvice = async () => {
   try {
     const url = "https://api.adviceslip.com/advice";
@@ -11,6 +13,7 @@ const fetchAdvice = async () => {
   }catch(err) {
     console.error(err);
   }
-}    
+}
 
+hamburger.addEventListener("click", () => dropdown.classList.toggle("active"));
 window.addEventListener("load", fetchAdvice);
