12193b467cb3b03f9135f31829dfb1a131037421
purplehippo911 <76628594+purplehippo911@users.noreply.github.com>
2023-03-12 17:06:13 +0000

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
+import "../styles/components/_footer";
+
 const Footer = () => {
     return ( 
         <footer className="footer">
             <div className="container grid">
-                <a href="https://github.com/purplehippo911/" className="hero__item Github">
-                    <img src="/src/assets/images/github.png" alt="Check me out on Github" />
-                    <em>Don't forget to check me out on github </em>
+                <a href="https://github.com/purplehippo911/" className="hero__item">
+                    <img src="/src/assets/github.svg" alt="Check me out on Github" className="github" />
+                </a>
+                <a href="https://twitter.com/muggie43/" className="hero__item">
+                    <img src="/src/assets/twitter.svg" alt="Check me out on Twitter" className="twitter"/>
                 </a>
             </div>
         </footer>
